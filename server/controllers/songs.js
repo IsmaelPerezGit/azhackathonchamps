0e7ae0941e758c65d6f3d72e73b0b119386c550e
Troy Amelotte <troy.amelotte@gTAef49b1.local>
2017-07-15 10:44:23 -0700

    made song controller

---
+++ server/controllers/songs.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose');
+let request = require('request');
+let User = mongoose.model('User');
+module.exports = {
+  index: (req, res)=>{
+
+  },
+
+}
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,5 +14,6 @@ module.exports = {
         res.status(200).send();
       }
     })
-  }
+  },
+
 }
